f77b64cb9a3323d6dfc053563435e98d2e67952a
Cardoso <ajca.dev@gmail.com>
2024-05-30 09:38:13 -0400

    add event router

---
+++ src/server/api/routers/event.ts
@@ -0,0 +1,37 @@
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { EventSchema, IdSchema } from "~/lib/schemas/event-schema";
+
+export const eventRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    return ctx.db.event.findMany();
+  }),
+
+  getOne: publicProcedure.input(IdSchema).query(async ({ ctx, input }) => {
+    return await ctx.db.event.findFirst({
+      where: {
+        id: input.id,
+      },
+    });
+  }),
+
+  createBibleStudy: publicProcedure
+    .input(EventSchema)
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.event.create({
+        data: {
+          name: input.name,
+          code: input.code,
+        },
+      });
+    }),
+
+  deleteBibleStudy: publicProcedure
+    .input(IdSchema)
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.event.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+});
