0c8d4aec506365b50d944dfae919ad2a70c030cb
Cardoso <ajca.dev@gmail.com>
2024-05-30 09:23:53 -0400

    add schema event

---
+++ src/lib/schemas/event-schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const IdSchema = z.object({ id: z.number() });
+export const EventSchema = z.object({
+  code: z.string(),
+  name: z.string().min(3, { message: "Name too short." }),
+});
+
+export type IdSchema = z.infer<typeof IdSchema>;
+export type EventSchema = z.infer<typeof EventSchema>;
